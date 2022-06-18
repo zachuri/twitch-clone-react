9ff4ea9ae070256b0dc3cf2217f54b353efe1454
zachuri <punsalang.zachary@gmail.com>
2022-06-18 16:59:52 -0700

    remove .env/ auth working

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { FaGithub } from "react-icons/fa";
 
-const account = () => {
+const Account = () => {
 	const { data: session } = useSession();
 	if (session) {
 		return (
-			<>
+			<div>
 				Signed in as {session.user.email} <br />
 				<button onClick={() => signOut()}>Sign out</button>
-			</>
+			</div>
 		);
 	}
 	return (
 		<div className="pt-[100px] flex flex-col max-w-[400px] w-full mx-auto p-4">
-			Not signed in <br />
-			<button onClick={() => signIn()}>Sign in</button>
+			<h2 className="text-3xl font-bold">Login</h2>
+			<p className="py-4">Choose the account you want to sign in with.</p>
+			<button
+				className="flex items-center justify-center p-3 bg-gray-600 border border-gray-600 my-2"
+				onClick={() => signIn()}
+			>
+				<FaGithub className="mr-2"></FaGithub>
+				Sign in<span className="font-bold pl-1">Github</span>
+			</button>
 		</div>
 	);
 };
 
-export default account;
+export default Account;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
@@ -7,10 +8,10 @@ export default NextAuth({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
 		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		}),
+		// GoogleProvider({
+		// 	clientId: process.env.GOOGLE_CLIENT_ID,
+		// 	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		// }),
 		// ...add more providers here
 	],
 	secret: process.env.JWT_SECRET,
