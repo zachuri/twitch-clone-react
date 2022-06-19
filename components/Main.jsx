6354346a13836893c0d37376e10938cb05b167f7
zachuri <punsalang.zachary@gmail.com>
2022-06-19 00:32:43 -0700

    completed adding youtube vid as hero/ flex

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LiveChannels from "../components/LiveChannels";
+import Hero from "../components/Hero";
 
 const Main = () => {
 	return (
 		<div className="absolute left-[64px] xl:left-[220px]">
+			<Hero />
 			<LiveChannels />
 		</div>
 	);
