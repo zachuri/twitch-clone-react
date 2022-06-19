649b103bafa8cac8253fb19c22507c19d9f4f5f6
zachuri <punsalang.zachary@gmail.com>
2022-06-18 17:26:55 -0700

    completed github auth

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
 
 const Account = () => {
 	const { data: session } = useSession();
+	// console.log(session);
+
 	if (session) {
 		return (
-			<div>
-				Signed in as {session.user.email} <br />
-				<button onClick={() => signOut()}>Sign out</button>
+			<div className="pt-[100px] flex flex-col max-w-[400px] w-full mx-auto p-4">
+				<h2 className="text-2xl font-bold">Welcome, {session.user.name}</h2>
+				<p className="py-4">Signed in as {session.user.email}</p>
+				<div className="pb-4 m-auto">
+					<Image
+						src={session.user.image}
+						alt="/"
+						width="100"
+						height="100"
+						className="rounded-full"
+					/>
+				</div>
+				<button
+					className="flex items-center justify-center p-3 bg-gray-600 border-gray-600"
+					onClick={() => signOut()}
+				>
+					Sign out
+				</button>
 			</div>
 		);
 	}
