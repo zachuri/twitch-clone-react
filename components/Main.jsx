54cc21bd2762a683d9a8ec2b01bb9ec75b677b87
zachuri <punsalang.zachary@gmail.com>
2022-06-19 00:43:22 -0700

    added icon bar/ next will be categories

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LiveChannels from "../components/LiveChannels";
 import Hero from "../components/Hero";
+import IconBar from "../components/IconBar";
 
 const Main = () => {
 	return (
 		<div className="absolute left-[64px] xl:left-[220px]">
 			<Hero />
 			<LiveChannels />
+			<IconBar />
 		</div>
 	);
 };
