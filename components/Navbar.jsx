e51bc016dbc676ad3f78f86b3d634984c20e2335
zachuri <punsalang.zachary@gmail.com>
2022-06-18 18:04:37 -0700

    completed navbar (added profile picture/ menu bar for logout)/ added break points for css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,7 @@ import Logo from "../public/assets/logo.png";
 import Image from "next/image";
 import { BsThreeDotsVertical, BsSearch, BsPerson } from "react-icons/bs";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
@@ -16,6 +17,10 @@ const Navbar = () => {
 	const handleNav = () => {
 		setNav(!nav);
 	};
+
+	const { data: session } = useSession();
+
+	console.log(session);
 
 	return (
 		<div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10">
@@ -125,14 +130,86 @@ const Navbar = () => {
 
 			{/* Right Side */}
 			<div className="hidden md:flex grow items-center justify-end">
-				<div className="flex items-center">
-					<Link href="/account">
-						<button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147ff]">
-							Account
-						</button>
-					</Link>
-					<BsPerson size={30} />
-				</div>
+				{session ? (
+					<div className="flex items-center">
+						<Link href="/account">
+							<div>
+								<p className="pr-4 cursor-pointer">
+									Welcome, {session.user.name}
+								</p>
+							</div>
+						</Link>
+						{/* Headless UI */}
+						<Menu as="div" className="relative text-left">
+							<div className="flex">
+								<Menu.Button>
+									<Image
+										className="rounded-full"
+										src={session.user.image}
+										width="40"
+										height="40"
+									/>
+								</Menu.Button>
+							</div>
+
+							<Transition
+								as={Fragment}
+								enter="transition ease-out duration-100"
+								enterFrom="transform opacity-0 scale-95"
+								enterTo="transform opacity-100 scale-100"
+								leave="transition ease-in duration-75"
+								leaveFrom="transform opacity-100 scale-100"
+								leaveTo="transform opacity-0 scale-95"
+							>
+								{/* change direction to show on the left (right-0) */}
+								<Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none">
+									<div className="py-1">
+										<Menu.Item>
+											{({ active }) => (
+												// eslint-disable-next-line @next/next/no-html-link-for-pages
+												<a
+													href="/account"
+													className={classNames(
+														active
+															? "bg-gray-500 text-gray-100"
+															: "text-gray-200",
+														"block px-4 py-2 text-sm"
+													)}
+												>
+													Account
+												</a>
+											)}
+										</Menu.Item>
+										<Menu.Item>
+											{({ active }) => (
+												<p
+													onClick={() => signOut()}
+													className={classNames(
+														active
+															? "bg-gray-500 text-gray-100"
+															: "text-gray-200",
+														"block px-4 py-2 text-sm"
+													)}
+												>
+													Logout
+												</p>
+											)}
+										</Menu.Item>
+									</div>
+								</Menu.Items>
+							</Transition>
+						</Menu>
+					</div>
+				) : (
+					<div className="flex items-center">
+						<Link href="/account">
+							<button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147ff]">
+								Account
+							</button>
+						</Link>
+						<BsPerson size={30} />
+					</div>
+				)}
 			</div>
 
 			{/* Hamburger Menu */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1028px',
+      // => @media (min-width: 1028px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1600px',
+      // => @media (min-width: 1600px) { ... }
+
+      '3xl': '1800px',
+      // => @media (min-width: 1800px) { ... }
+    }
   },
   plugins: [],
 }
